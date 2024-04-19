96142d43832b077b94bf2d0e5d8509335307cb43
Alexander Shilov <tabbols95@gmail.com>
2024-04-19 20:21:33 +0300

    Update invest.py

--- invest.py
+++ invest.py
@@ -1,16 +1,47 @@
-from typing import Optional
+from typing import Optional, Union
+import pandas as pd
+from datetime import datetime as dt
+from datetime import timedelta as td
+from datetime import date
 
 
 class Bond:
     """Класс облигаций"""
+
+    Percent = float
     
     def __init__(self,
                  nominal: float,
-                 price: float,
-                 isin: Optional[str] = None):
+                 market_price: float,
+                 date_of_maturity: Union[dt, str],
+                 nkd: Optional[float] = None,
+                 calculation_date: Optional[dt] = None,
+                 basis: int = 365):
         self.nominal = nominal
-        self.price = price
-        self.isin = isin
-        self.discount = self.nominal - self.price if self.nominal - self.price > 0 else 0
-        self.premium = self.price - self.nominal if self.price - self.nominal > 0 else 0
-        pass
+        """Номинальная стоимость облигации"""
+
+        self.market_price = market_price
+        """Рыночная стоимость облигации"""
+
+        self.date_of_maturity = date_of_maturity if isinstance(date_of_maturity, date) else dt.fromisoformat(date_of_maturity)
+        """Дата погашения облигации / Дата оферты"""
+
+        self.nkd = nkd
+        """НКД"""
+
+        self.calculation_date = dt.now().date() if calculation_date is None else calculation_date
+        """Дата расчета"""
+
+        self.term = (self.date_of_maturity - self.calculation_date).days
+        """Срок до погашения"""
+
+        self.basis = basis
+        """Базис"""
+    
+    def get_simple_yield(self,
+                         coupons: pd.Series) -> Percent:
+        Y = round(((self.nominal - self.market_price)/self.market_price)*(self.basis/self.term)*100, 3)
+        """Доходность бескупонной облигации"""
+
+        if self.nkd is None:
+            return Y
